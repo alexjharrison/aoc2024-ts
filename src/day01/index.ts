f9bceb0692441ce0168f8b0d31e82a5b7d9fce96
Alex Harrison <alexjharrison@gmail.com>
2024-12-01 17:32:05 -0500

    fix prettier settings

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,30 +1,30 @@
 import run from "aocrunner"
 
-const parseInput = (rawInput: string) => rawInput.split("\n").map(line=>line.split('  ').map(Number))
+const parseInput = (rawInput: string) => rawInput.split("\n").map(line => line.split("  ").map(Number))
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput)
-  const list1 = input.map(val=>val[0]).sort()
-  const list2 = input.map(val=>val[1]).sort()
-  let sum  = 0
-  for(let i = 0;i<list1.length;i++){
-    sum += Math.abs(list1[i] - list2[i]) 
+  const list1 = input.map(val => val[0]).sort()
+  const list2 = input.map(val => val[1]).sort()
+  let sum = 0
+  for (let i = 0; i < list1.length; i++) {
+    sum += Math.abs(list1[i] - list2[i])
   }
   return sum
 }
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput)
-  const left = input.map(val=>val[0])
-  
-  const right = input.map(val=>val[1])
+  const left = input.map(val => val[0])
+
+  const right = input.map(val => val[1])
   let sum = 0
 
-  for(const val of left){
+  for (const val of left) {
     let localSum = 0
-    for(const valr of right){
-      if(val === valr){
-        localSum+=val
+    for (const valr of right) {
+      if (val === valr) {
+        localSum += val
       }
     }
     sum += localSum
@@ -36,17 +36,17 @@ const part2 = (rawInput: string) => {
 run({
   part1: {
     tests: [
-//       {
-//         input: `
-// 3   4
-// 4   3
-// 2   5
-// 1   3
-// 3   9
-// 3   3
-//         `,
-//         expected: 11,
-//       },
+      //       {
+      //         input: `
+      // 3   4
+      // 4   3
+      // 2   5
+      // 1   3
+      // 3   9
+      // 3   3
+      //         `,
+      //         expected: 11,
+      //       },
     ],
     solution: part1,
   },
