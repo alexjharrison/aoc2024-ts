87835bf8e3cf0356fc2e894b7d966d565589e7c1
Alex Harrison <alexjharrison@gmail.com>
2024-12-01 19:25:37 -0500

    add hashmap util func

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,13 +1,10 @@
 export const sum = (arr: number[]) => arr.reduce((sum, val) => sum + val, 0)
-export const multiply = (arr: number[]) =>
-  arr.reduce((sum, val) => sum * val, 1)
+export const multiply = (arr: number[]) => arr.reduce((sum, val) => sum * val, 1)
 
-export const gcd = (a: number, b: number): number =>
-  b == 0 ? a : gcd(b, a % b)
+export const gcd = (a: number, b: number): number => (b == 0 ? a : gcd(b, a % b))
 export const lcm = (a: number, b: number) => (a / gcd(a, b)) * b
 export const lcmAll = (ns: number[]) => ns.reduce(lcm, 1)
-export const rng = (lo: number, hi: number) =>
-  [...Array(hi - lo + 1)].map((_, i) => lo + i)
+export const rng = (lo: number, hi: number) => [...Array(hi - lo + 1)].map((_, i) => lo + i)
 
 export const mod = (n: number, m: number) => ((n % m) + m) % m
 
@@ -27,11 +24,9 @@ export const setIntersection = <T>(sets: Set<T>[]): Set<T> => {
   return output
 }
 
-export const areArrsEq = <T>(arr1: T[], arr2: T[]) =>
-  arr1.every(val => arr2.includes(val))
+export const areArrsEq = <T>(arr1: T[], arr2: T[]) => arr1.every(val => arr2.includes(val))
 
-export const enumerate = <T>(arr: T[]): [number, T][] =>
-  arr.map((val, i) => [i, val])
+export const enumerate = <T>(arr: T[]): [number, T][] => arr.map((val, i) => [i, val])
 
 export const cypherChar = (char: string, offset: number) => {
   const lower = char.toLowerCase()
@@ -47,4 +42,13 @@ export const cypherStr = (str: string, offset: number) => {
     newStr += cypherChar(letter, offset)
   }
   return newStr
+}
+
+export const createHashmap = (list: (string | number)[]): Record<string | number, number> => {
+  const hash: Record<string | number, number> = {}
+  for (const item of list) {
+    if (hash[item]) hash[item]++
+    else hash[item] = 1
+  }
+  return hash
 }
