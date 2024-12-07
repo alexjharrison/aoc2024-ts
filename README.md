b441f50c5cf80d404a5579996bf2cffbcb4d5f95
Alex Harrison <alexjharrison@gmail.com>
2024-12-07 15:03:43 -0500

    solve day 7

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
-![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
@@ -111,9 +111,9 @@ Both parts: 899.934ms
 
 ```
 Day 07
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 6.971ms
+Time part 2: 502.511ms
+Both parts: 509.482ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 12/50
-Total time: 1012.73ms
+Total stars: 14/50
+Total time: 1522.212ms
 ```
 
 <!--/RESULTS-->
--- src/day07/index.ts
+++ src/day07/index.ts
@@ -7,39 +7,81 @@ const parseInput = (rawInput: string) => {
   })
 }
 
-const combinate = (target: number, vals: number[]): Set<number> => {
-  let set = new Set<number>([vals[0]])
-  for (let i = 1; i < vals.length; i++) {
-    for (const val of set) {
-      if (val > target) continue
-      set = set.union(combos(val, vals[i]))
-    }
-  }
-  return set
-}
-
-const combos = (a: number, b: number): Set<number> => {
-  return new Set([a + b, a * b])
-}
-
 const part1 = (rawInput: string) => {
   const lines = parseInput(rawInput)
-  let count = 0
+  let sum = 0
   for (const { test, operators } of lines) {
-    const set = combinate(test, operators)
-    if (set.has(test)) count += test
+    sum += check(test, operators.slice(1), operators[0])
   }
-  return count
+  return sum
+
+  function check(target: number, list: number[], current: number): number {
+    const [next, ...rest] = list
+    const sum = current + next
+    const mult = current * next
+    if (rest.length === 0) {
+      if (sum === target) return sum
+      if (mult === target) return mult
+      return 0
+    }
+    const sumBranch = check(target, rest, sum)
+    if (sumBranch) return sumBranch
+    const multBranch = check(target, rest, mult)
+    if (multBranch) return multBranch
+    return 0
+  }
 }
 
 const part2 = (rawInput: string) => {
   const lines = parseInput(rawInput)
+  let sum = 0
+  for (const { test, operators } of lines) {
+    sum += check(test, operators.slice(1), operators[0])
+  }
+  return sum
 
-  return
+  function check(target: number, list: number[], current: number): number {
+    const [next, ...rest] = list
+    const sum = current + next
+    const mult = current * next
+    const fuze = Number(current + "" + next)
+    if (rest.length === 0) {
+      if (sum === target) return sum
+      if (mult === target) return mult
+      if (fuze === target) return fuze
+      return 0
+    }
+    const sumBranch = check(target, rest, sum)
+    if (sumBranch) return sumBranch
+    const multBranch = check(target, rest, mult)
+    if (multBranch) return multBranch
+    const fuzeBranch = check(target, rest, fuze)
+    if (fuzeBranch) return fuzeBranch
+    return 0
+  }
 }
 
 run({
   part1: {
+    tests: [
+      // {
+      //   input: `
+      //   190: 10 19
+      //   3267: 81 40 27
+      //   83: 17 5
+      //   156: 15 6
+      //   7290: 6 8 6 15
+      //   161011: 16 10 13
+      //   192: 17 8 14
+      //   21037: 9 7 18 13
+      //   292: 11 6 16 20
+      //   `,
+      //   expected: 3749,
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
     tests: [
       {
         input: `
@@ -53,17 +95,8 @@ run({
         21037: 9 7 18 13
         292: 11 6 16 20
         `,
-        expected: 3749,
+        expected: 11387,
       },
-    ],
-    solution: part1,
-  },
-  part2: {
-    tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
     ],
     solution: part2,
   },
